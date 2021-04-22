f777ead667834913dc1cd5eeb2bc651a55d43a6d
voidweaver <44946758+voidweaver@users.noreply.github.com>
2021-04-22 16:14:40 +0700

    Retrieve data in the page instead of components

--- components/ResourceBars.tsx
+++ components/ResourceBars.tsx
@@ -1,6 +1,4 @@
-import { FC, useState, useEffect } from "react";
-
-import MinesinAPI from "../lib/api";
+import { FC } from "react";
 
 import Icon from "@mdi/react";
 import { mdiCircle } from "@mdi/js";
@@ -8,7 +6,10 @@ import { mdiCircle } from "@mdi/js";
 import ProgressBar from "./ProgressBar";
 
 interface ResourceBarProps {
-  api: MinesinAPI | undefined;
+  usages: {
+    cpu: number;
+    ram: number;
+  };
 }
 
 const colorBreakpoints = [
@@ -27,58 +28,32 @@ const getBreakpoint = (value: number) => {
   return colorBreakpoints[colorBreakpoints.length - 1];
 };
 
-const ResourceBar: FC<ResourceBarProps> = ({ api }) => {
-  const [cpuUsage, setCPUUsage] = useState(0);
-  const [ramUsage, setRAMUsage] = useState(0);
-
-  useEffect(() => {
-    if (api === undefined) return;
-
-    const cpuUsagePromise = api.getCPUUsage();
-    const ramUsagePromise = api.getRAMUsage();
-
-    cpuUsagePromise.promise
-      .then((usage) => setCPUUsage(usage ?? 0))
-      .catch((reason) => {
-        if (reason.isCancelled !== true) throw reason;
-      });
-    ramUsagePromise.promise
-      .then((usage) => setRAMUsage(usage ?? 0))
-      .catch((reason) => {
-        if (reason.isCancelled !== true) throw reason;
-      });
-
-    return () => {
-      cpuUsagePromise.cancel();
-      ramUsagePromise.cancel();
-    };
-  }, [api]);
-
-  const cpuColor = getBreakpoint(cpuUsage);
-  const ramColor = getBreakpoint(ramUsage);
+const ResourceBar: FC<ResourceBarProps> = ({ usages }) => {
+  const cpuColor = getBreakpoint(usages.cpu);
+  const ramColor = getBreakpoint(usages.ram);
 
   return (
-    <div className="bg-white text-black p-4 rounded-md max-w-md">
+    <div className="bg-white text-black p-4 rounded-md">
       <h2 className="text-2xl font-bold mb-3">Resources</h2>
       <p className="font-medium">CPU Usage</p>
-      <ProgressBar color={cpuColor.bg} progress={cpuUsage} />
+      <ProgressBar color={cpuColor.bg} progress={usages.cpu} />
       <p className="mb-2">
         <Icon
           path={mdiCircle}
           size="0.8em"
           className={`inline ${cpuColor.text} transform -translate-y-0.5`}
         />{" "}
-        Used: {cpuUsage.toFixed(2)}%
+        Used: {usages.cpu.toFixed(2)}%
       </p>
       <p className="font-medium">RAM Usage</p>
-      <ProgressBar color={ramColor.bg} progress={ramUsage} />
+      <ProgressBar color={ramColor.bg} progress={usages.ram} />
       <p className="mb-2">
         <Icon
           path={mdiCircle}
           size="0.8em"
           className={`inline ${ramColor.text} transform -translate-y-0.5`}
         />{" "}
-        Used: {ramUsage.toFixed(2)}%
+        Used: {usages.ram.toFixed(2)}%
       </p>
     </div>
   );
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import Layout from "../../components/Layout";
 import { dashboardNavs } from "../../lib/navs";
-import MinesinAPI from "../../lib/api";
+import MinesinAPI, { MembersArray } from "../../lib/api";
 
 import MembersList from "../../components/MembersList";
 import ResourceBars from "../../components/ResourceBars";
@@ -11,12 +11,34 @@ import { useEffect, useState } from "react";
 export default function Dashboard() {
   const router = useRouter();
 
-  const [api, setAPI] = useState<MinesinAPI | undefined>(undefined);
+  const [api, setAPI] = useState<MinesinAPI>();
+  const [members, setMembers] = useState<MembersArray>();
+  const [cpu, setCPU] = useState<number>();
+  const [ram, setRAM] = useState<number>();
 
-  useEffect(
-    () => setAPI(new MinesinAPI(localStorage.getItem("accessToken") ?? "", router, true)),
-    []
-  );
+  const ignoreCanceled = (reason: { isCancelled: boolean } | any) => {
+    if (reason.isCanceled !== true) throw reason;
+  };
+
+  useEffect(() => {
+    const api = new MinesinAPI(localStorage.getItem("accessToken") ?? "", router, true);
+    setAPI(api);
+    const promises = {
+      cpu: api.getCPUUsage(),
+      ram: api.getRAMUsage(),
+      members: api.getMembers(),
+    };
+
+    promises.cpu.promise.then(setCPU);
+    promises.ram.promise.then(setRAM);
+    promises.members.promise.then(setMembers);
+
+    Object.values(promises).forEach((promise) => promise.promise.catch(ignoreCanceled));
+
+    return () => Object.values(promises).forEach((promise) => promise?.cancel());
+  }, []);
+
+  // TODO: periodically request new data from API
 
   return (
     <Layout
@@ -24,8 +46,13 @@ export default function Dashboard() {
       color="bg-gray-900"
       className="text-white m-4 grid grid-cols-1 auto-rows-min gap-4"
     >
-      <MembersList api={api} />
-      <ResourceBars api={api} />
+      <MembersList members={members ?? []} />
+      <ResourceBars
+        usages={{
+          cpu: cpu ?? 0,
+          ram: ram ?? 0,
+        }}
+      />
     </Layout>
   );
 }
