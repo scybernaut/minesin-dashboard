d9f428d627ffdcdc7850dae90918bab601b3b0ed
scybernaut <84150508+scybernaut@users.noreply.github.com>
2021-06-07 17:23:10 +0700

    Added member list to dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,21 +1,29 @@
 import Layout from "../../components/Layout";
 import { dashboardNavs } from "../../lib/navs";
+import { loggedOutReasons } from "../../lib/shorthands";
+
+import MembersList from "../../components/MembersList";
 
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
+export type LoggedOutReasonCodes = keyof typeof loggedOutReasons;
+
 export default function Dashboard() {
   const router = useRouter();
+  const authRedirect = (reason?: LoggedOutReasonCodes) => {
+    if (reason === "invalid_token") localStorage.removeItem("accessToken");
+    router.replace(`/dashboard/auth${reason ? "?reason=" + encodeURIComponent(reason) : ""}`);
+  };
+
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
-    if (accessToken === null) router.replace("/dashboard/auth");
+    if (accessToken === null) authRedirect();
   }, []);
 
   return (
-    <Layout
-      navs={dashboardNavs}
-      color="bg-gray-900"
-      className="text-white flex flex-col justify-center"
-    ></Layout>
+    <Layout navs={dashboardNavs} color="bg-gray-900" className="text-white m-4">
+      <MembersList onInvalidToken={authRedirect}></MembersList>
+    </Layout>
   );
 }
