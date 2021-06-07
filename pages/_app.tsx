4bfb2544a5e63fa9907793d3af142129570db658
scybernaut <84150508+scybernaut@users.noreply.github.com>
2021-06-07 17:42:09 +0700

    Added page title and meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,20 @@ import "../styles/tailwind.css";
 
 import React from "react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>MINESIN Dashboard</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
