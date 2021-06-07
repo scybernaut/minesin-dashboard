4d534342696356806858e5fec82b709c13db020c
scybernaut <84150508+scybernaut@users.noreply.github.com>
2021-06-07 17:42:12 +0700

    Fixed warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
     <>
       <Head>
         <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>MINESIN Dashboard</title>
       </Head>
