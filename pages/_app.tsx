7b347d3454166fc10eb1b8d4061888cdf66a8838
scybernaut <84150508+scybernaut@users.noreply.github.com>
2021-06-07 17:42:12 +0700

    Minor refinements and improve a11y

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,8 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="The superior version of MINESIN dashboard" />
+        <meta name="author" content="VOIDWEAVER" />
         <title>MINESIN Dashboard</title>
       </Head>
       <Component {...pageProps} />
