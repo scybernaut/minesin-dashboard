5aed49c89903efdcbe0d5780101a091a9153e6cb
scybernaut <84150508+scybernaut@users.noreply.github.com>
2021-06-07 17:42:03 +0700

    Added member list to dashboard

--- lib/shorthands.ts
+++ lib/shorthands.ts
@@ -1,2 +1,7 @@
-export const focusRing =
-  "focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-70";
+export const focusRing = "focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-70";
+
+export const apiBase = "https://omsinkrissada.sytes.net/api/minecraft/";
+
+export const loggedOutReasons: Record<string, string> = {
+  invalid_token: "Your session has expired or is invalid.",
+};
